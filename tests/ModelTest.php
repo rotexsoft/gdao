8ab855d452207acecd57b40efe222ccd612dc0f7
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2015-07-23 11:30:43 -0600

    Wrote more unit tests.

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- * Description of ModelTest
+ * Tests all testable methods in GDAO\Model via a child class 
+ * (\MockModelForTestingNonAbstractMethods).
  *
  * @author aadegbam
  */
@@ -11,6 +12,7 @@ class ModelTest extends \PHPUnit_Framework_TestCase
     protected $_mock_model_obj_with_memory_sqlite_connection;
 
     protected function setUp() {
+        
         parent::setUp();
 
         $this->_mock_model_obj_with_no_db_connection = 
@@ -115,7 +117,7 @@ class ModelTest extends \PHPUnit_Framework_TestCase
                 '',
                 '',
                 '',
-                [],//[PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'],
+                [],
                 [
                     '_primary_col'=>'component_id', 
                     '_table_name'=>'components', 
@@ -137,7 +139,7 @@ class ModelTest extends \PHPUnit_Framework_TestCase
                 '',
                 '',
                 '',
-                [],//[PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'],
+                [],
                 [
                     'primary_col'=>'component_id', 
                     'table_name'=>'components', 
@@ -1783,7 +1785,7 @@ class ModelTest extends \PHPUnit_Framework_TestCase
 // End of Tests for \GDAO\Model::_validateWhereOrHavingParamsArray(array $array)
 ////////////////////////////////////////////////////////////////////////////////
     
-    public function testToEnsureThatGetWhereOrHavingClauseWithParamsWorksAsExpected() {
+    public function testThatGetWhereOrHavingClauseWithParamsWorksAsExpected() {
         
         $data = [
             'where' => 
@@ -1855,7 +1857,7 @@ EOT;
         $this->assertEquals($expected_params, $result[1]);
     }
     
-    public function testToEnsureThatToStringWorksAsExpected() {
+    public function testThat__toStringWorksAsExpected() {
         
         $model = new \MockModelForTestingNonAbstractMethods(
             'test_dsn',
@@ -1897,6 +1899,38 @@ Array
 EOT;
         //echo $model->__toString(); exit;
         $this->assertContains($sub_str, $model->__toString());
+    }
+    
+    public function testThatToArrayWorksAsExpected() {
+        
+        $model = new \MockModelForTestingNonAbstractMethods(
+            'test_dsn',
+            'test_username',
+            'test_passwd',
+            [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'],
+            [
+                '_primary_col'=>'component_id', 
+                '_table_name'=>'components',
+            ]
+        );
+        
+        $expected_array = [
+            '_primary_col' => 'component_id',
+            '_table_name' => 'components',
+            '_table_cols' => [],
+            '_collection_class_name' => null,
+            '_record_class_name' => null,
+            '_created_timestamp_column_name' => null,
+            '_updated_timestamp_column_name' => null,
+            '_relations' => [],
+            '_dsn' => 'test_dsn',
+            '_username' => 'test_username',
+            '_passwd' => 'test_passwd',
+            '_pdo_driver_opts' => [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']
+        ];
+
+        $msg = __METHOD__;
+        $this->assertTrue( $expected_array === $model->toArray(), $msg);
     }
     
     /**
@@ -1975,7 +2009,158 @@ EOT;
         $model->fetchRecordsIntoCollection();
     }
     
+    public function testThatGetCreatedTimestampColumnNameWorksAsExpected() {
+
+        //create model setting property values with exact property names
+        $model = new \MockModelForTestingNonAbstractMethods(
+                    '', '', '', [],
+                    [
+                        '_primary_col'=>'component_id', 
+                        '_table_name'=>'components',
+                        '_created_timestamp_column_name'=> 'test_c_col_name',
+                    ]
+                );
+        $msg = __METHOD__;
+        $this->assertTrue($model->getCreatedTimestampColumnName() === 'test_c_col_name', $msg);
+    }
+    
+    public function testThatGetUpdatedTimestampColumnNameWorksAsExpected() {
+
+        //create model setting property values with exact property names
+        $model = new \MockModelForTestingNonAbstractMethods(
+                    '', '', '', [],
+                    [
+                        '_primary_col'=>'component_id', 
+                        '_table_name'=>'components',
+                        '_updated_timestamp_column_name'=> 'test_u_col_name',
+                    ]
+                );
+        $msg = __METHOD__;
+        $this->assertTrue($model->getUpdatedTimestampColumnName() === 'test_u_col_name', $msg);
+    }
+    
+    public function testThatGetPrimaryColNameWorksAsExpected() {
+
+        //create model setting property values with exact property names
+        $model = new \MockModelForTestingNonAbstractMethods(
+                    '', '', '', [],
+                    [
+                        '_primary_col'=>'component_id', 
+                        '_table_name'=>'components',
+                    ]
+                );
+        $msg = __METHOD__;
+        $this->assertTrue($model->getPrimaryColName() === 'component_id', $msg);
+        $this->assertTrue($model->getPrimaryColName(true) === 'components.component_id', $msg);
+    }
+    
+    public function testThatGetTableNameWorksAsExpected() {
+
+        //create model setting property values with exact property names
+        $model = new \MockModelForTestingNonAbstractMethods(
+                    '', '', '', [],
+                    [
+                        '_primary_col'=>'component_id', 
+                        '_table_name'=>'components',
+                    ]
+                );
+        $msg = __METHOD__;
+        $this->assertTrue($model->getTableName() === 'components', $msg);
+    }
+    
+    public function testThatGetTableColNamesWorksAsExpected() {
+        
+        //create model setting property values with exact property names
+        $model = new \MockModelForTestingNonAbstractMethods(
+                '',
+                '',
+                '',
+                [],
+                [
+                    '_primary_col'=>'component_id', 
+                    '_table_name'=>'components',
+                    '_table_cols'=>['col1', 'col2'],
+                ]
+            );
+        $model2 = new \MockModelForTestingNonAbstractMethods(
+                '',
+                '',
+                '',
+                [],
+                [
+                    '_primary_col'=>'component_id', 
+                    '_table_name'=>'components',
+                    '_table_cols'=>[
+                                    'col1'=>['sub array with metadata for col1'], 
+                                    'col2'=>['sub array with metadata for col2']
+                                ],
+                ]
+            );
+        $msg = __METHOD__;
+        $this->assertTrue(['col1', 'col2'] === $model->getTableColNames(), $msg);
+        $this->assertTrue(['col1', 'col2'] === $model2->getTableColNames(), $msg);
+    }
+    
+    public function testThatGetRelationNamesWorksAsExpected() {
+
+        $rel = [
+            'a_relation_name'=> [
+                'relation_type' => \GDAO\Model::RELATION_TYPE_BELONGS_TO,
+                'foreign_key_col_in_my_table' => 'output_id',
+                'foreign_table' => 'project_outputs',
+                'foreign_key_col_in_foreign_table' => 'output_id',
+                'primary_key_col_in_foreign_table' => 'output_id',
+                'foreign_models_class_name' => '\\VendorName\\PackageName\\ModelClassName',
+                'foreign_models_collection_class_name' => '\\VendorName\\PackageName\\ModelClassName\\Collection',
+                'foreign_models_record_class_name' => '\StdClass',
+                'foreign_table_sql_params' => [
+                    'cols' => ['project_outputs.deliverable_id', 'component_deliverables.deliverable', 'component_deliverables.component_id'],
+                    'where' => [
+                        [ 'col' => 'project_outputs.hidden_fiscal_year', 'op' => '=', 'val' => 16 ],
+                        [ 'col' => 'project_outputs.deactivated', 'op' => '=', 'val' => 0],
+                        [ 'col' => 'project_outputs.parent_id', 'op' => 'is-null'],
+                    ],
+                ]
+            ],
+            'a_relation_name2'=> [
+                'relation_type' => \GDAO\Model::RELATION_TYPE_HAS_MANY,
+                'foreign_key_col_in_my_table' => 'output_id',
+                'foreign_table' => 'project_outputs',
+                'foreign_key_col_in_foreign_table' => 'output_id',
+                'primary_key_col_in_foreign_table' => 'output_id',
+                'foreign_models_class_name' => '\\VendorName\\PackageName\\ModelClassName',
+                'foreign_models_collection_class_name' => '\\VendorName\\PackageName\\ModelClassName\\Collection',
+                'foreign_models_record_class_name' => '\StdClass',
+                'foreign_table_sql_params' => [
+                    'cols' => ['project_outputs.deliverable_id', 'component_deliverables.deliverable', 'component_deliverables.component_id'],
+                    'where' => [
+                        [ 'col' => 'project_outputs.hidden_fiscal_year', 'op' => '=', 'val' => 16 ],
+                        [ 'col' => 'project_outputs.deactivated', 'op' => '=', 'val' => 0],
+                        [ 'col' => 'project_outputs.parent_id', 'op' => 'is-null'],
+                    ],
+                ]
+            ],
+        ];
+        
+        //create model setting property values with exact property names
+        $model = new \MockModelForTestingNonAbstractMethods(
+                '',
+                '',
+                '',
+                [],
+                [
+                    '_primary_col'=>'component_id', 
+                    '_table_name'=>'components', 
+                    '_relations'=>$rel,
+                    '_table_cols'=>['col1', 'col2'],
+                ]
+            );
+        $msg = __METHOD__;
+        $this->assertTrue(['a_relation_name', 'a_relation_name2'] === $model->getRelationNames(), $msg);
+    }
+
     protected function _isHhvm() {
+        
         return defined('HHVM_VERSION');
     }
 }
